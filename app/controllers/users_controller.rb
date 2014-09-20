8eaac5cee3e8f865ff03951f355d3cbcc03759d6
Nabarun Banerjee <nbanerjee@kreeti.com>
2014-09-21 03:05:12 +0530

    admin user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signedin_user, only: [:index, :edit, :update, :destroy]
+  before_action :currect_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   def new
     @user = User.new
   end
@@ -14,12 +17,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if(@user.update_attributes(users_param))
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 
   private
@@ -27,4 +49,16 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  def signedin_user
+    redirect_to signin_url, notice: "Please signin" unless signed_in?
+  end
+
+  def currect_user
+    @user = User.find(params[:id])
+    redirect_to root_url, notice: "Not currect user" unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url, notice: "You are not admin" unless current_user.admin?
+  end
 end
